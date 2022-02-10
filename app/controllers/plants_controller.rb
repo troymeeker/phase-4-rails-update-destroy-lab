82e900b955fb2947e116613496336a144a4bdc8a
troymeeker <troymeeker1@gmail.com>
2022-02-10 12:31:30 -0800

    commit tree bf54662fbd557375c898a26ccda90515d01341cf

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,5 +1,21 @@
 class PlantsController < ApplicationController
 
+#patch 
+def update
+  plant = Plant.find_by(id: params[:id])
+  plant.update(plant_params)
+  render json: plant
+
+end 
+
+#delete
+def destroy
+  plant = Plant.find_by(id: params[:id])
+  plant.destroy
+  head :no_content
+
+end
+
   # GET /plants
   def index
     plants = Plant.all
@@ -18,9 +34,14 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  
+
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
+
+  
 end
